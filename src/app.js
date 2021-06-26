7cb84738f157762d188fd41ca79ac37b15de0782
Gaurav Gupta <gaurav1122gupta@gmail.com>
2021-06-27 03:41:05 +0530

    just for trial sake

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.get("/weather",(req,res)=>{
 })
 app.get("*",(req,res)=>{
     res.render("404error",{
-        errorMsg: 'OOPs ! Page Not Found'
+        errorMsg: 'OOPs ! Page Not Found :('
     })
 })
 
